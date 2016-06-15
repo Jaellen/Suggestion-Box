5b54f36066c17614f3d27e76b02743d2d9aebac3
Jonathan Aellen <Jon@JONATHANs-MacBook-Pro-4.local>
2016-06-15 00:03:12 -0400

    Adding service 'suggestions'

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,3 +1,6 @@
-app.controller('HomeController', ['$scope', function($scope) {
-  $scope.helloWorld = "Hello, AngularJS!";
+app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
+
+  $scope.hello = "Angular Test";
+  $scope.posts = suggestions.posts;
+
 }]);
---
+++ js/services/suggestions.js
@@ -0,0 +1,53 @@
+app.factory('suggestions', [function() {
+
+  var demoSuggestions =
+  {
+    posts:
+    [
+      {
+          title:"Save the environment by having everyone to bring their own mugs and water bottles to work",
+          upvotes:15,
+          comments:[]
+      },
+      {
+          title:"Hold a demo night social for everyone to show off their awesome work",
+          upvotes:11,
+          comments:[]
+      },
+      {
+
+          title:"Hold a demo night social for everyone to show off their awesome work",
+          upvotes:11,
+          comments:[]
+      },
+      {
+          title:"Implement a no-internal meeting day on Friday to allow employees to get more work done",
+          upvotes:9,
+          comments:[]
+      },
+      {
+          title:"Create an online swag shop for employees and customers to buy awesome company swag",
+          upvotes:8,
+          comments:[]
+      },
+      {
+          title:"Implemement an easier system for meeting and board room bookings",
+          upvotes:4,
+          comments:[]
+      },
+      {
+          title:"Offer a discount for yoga sessions",
+          upvotes:3,
+          comments:[]
+      },
+      {
+          title:"Paint the back wall with inspirational quotes and company values",
+          upvotes:1,
+          comments:[]
+      }
+    ]
+  };
+
+return demoSuggestions;
+
+}]);
