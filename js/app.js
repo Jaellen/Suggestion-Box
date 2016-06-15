40f1f410d79180203e08206ec4c2a42f50a44ee9
Jonathan Aellen <aellen.j@gmail.com>
2016-06-15 11:55:36 -0400

    Adding home view route

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('SuggestionBox', []);
+var app = angular.module('SuggestionBox', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+  	.when('/', {
+    controller: 'HomeController',
+    templateUrl: 'views/home.html'
+  	})
+
+  	.when('/comments/:id', {
+    controller: 'CommentsController',
+    templateUrl: 'views/comments.html'
+  	})
+
+  	.otherwise({
+  		redirectTo: '/'
+  	});
+});
