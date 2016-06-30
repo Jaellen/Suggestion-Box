e15af64549f2a43da0090049b7e5b8fdbcc6fe8d
Jonathan Aellen <aellen.j@gmail.com>
2016-06-30 12:39:28 -0400

    Setup local http-web-server

---
+++ build/app.bundle.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = angular.module('SuggestionBox', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider.when('/', {
+    controller: 'HomeController',
+    templateUrl: 'views/home.html'
+  }).when('/comments/:id', {
+    controller: 'CommentsController',
+    templateUrl: 'views/comments.html'
+  }).otherwise({
+    redirectTo: '/'
+  });
+});
