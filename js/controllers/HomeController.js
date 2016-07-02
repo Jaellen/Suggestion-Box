000f4d05e7be1501622908093dfd9cd4ba564d28
Jonathan Aellen <aellen.j@gmail.com>
2016-07-02 18:44:31 -0400

    Add delete and edit buttons

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,35 +1,29 @@
 app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
 
-  /* This is craeting an AngularJs object called 'a' which we are connecting the service
-  'suggestions' to and passing the data_array*/
   $scope.a = suggestions.data_array;
 
-  /* This function is used to add new suggestion to the data_array*/
-  $scope.addSuggestion = function()
-    {
-      //if input empty, don't submit
-      if(!$scope.title || $scope.title === "") {
-        return;
+  $scope.addSuggestion = function() {
+          //if input empty, don't submit
+          if(!$scope.title || $scope.title === "") {
+            return;
+          };
+
+          $scope.a.push({
+            title: $scope.title,
+            upvotes: 0,
+            comments: []
+          });
+
+          $scope.title = '';
+
       };
 
-console.log("TEST");
+    $scope.DeleteSuggestion = function(idx) {
+       $scope.a.splice(idx, 1);
+          };
 
-      //push new suggestion post into data_array
-      $scope.a.push({
-        title: $scope.title,  // here we are pushing the user input angular object a
-        upvotes: 0,
-        comments: []
-      });
-
-      //after submit, clear input
-      $scope.title = '';
-    };
-
-    // a function that increases the upvote by one each time clicked
     $scope.upVote = function(b) {
-      b.upvotes += 1;
+        b.upvotes += 1;
     };
-
-
 
 }]);
--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -1,50 +1,37 @@
 app.factory('suggestions', [function() {
 
-  //Array containing all the suggestions, upvotes and comments data
   var data_object =
   {
     data_array:
     [
       {
-
           title:"Hold a demo night social for everyone to show off their awesome work",
           upvotes:11,
-          comments:[]
-      },
-      {
-          title:"Implement a no-internal meeting day on Friday to allow employees to get more work done",
-          upvotes:9,
-          comments:[]
-      },
-      {
-          title:"Create an online swag shop for employees and customers to buy awesome company swag",
-          upvotes:8,
-          comments:[]
-      },
-      {
-          title:"Implement an easier system for meeting and board room bookings",
-          upvotes:4,
-          comments:[]
-      },
-      {
-          title:"Offer a discount for yoga sessions",
-          upvotes:3,
-          comments:[]
-      },
-      {
-          title:"Paint the back wall with inspirational quotes and company values",
-          upvotes:1,
           comments:[]
       },
       {
           title:"Save the environment by having everyone to bring their own mugs and water bottles to work",
           upvotes:15,
           comments:[]
+      },
+      {
+          title:"Implement a no-internal meeting day on Friday to allow employees to get more work done",
+          upvotes:9,
+          comments:[]
       }
     ]
   };
 
+  return data_object;
+
+  }]);
+
+  /* var data_object =
+  {
+    data_array:
+    [{}]
+  };
+
 return data_object;
 
-
-}]);
+}]); */
